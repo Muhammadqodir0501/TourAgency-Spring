478999c26d04ffb45df6909d34d283662aa26e51
Muhammadqodir <muhammadqodir0501@gmail.com>
2026-01-21 17:42:35 +0500

    Added JWT

--- src/main/java/org/example/touragency/config/PropertyConfig.java
+++ src/main/java/org/example/touragency/config/PropertyConfig.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import static org.example.touragency.constant.QueryConstants.PROPERTIES;
+
 @Configuration
 public class PropertyConfig {
+    public PropertyConfig(){}
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
         PropertySourcesPlaceholderConfigurer configurer =
                 new PropertySourcesPlaceholderConfigurer();
-        configurer.setLocation(new ClassPathResource("application.properties"));
+        configurer.setLocation(new ClassPathResource(PROPERTIES));
         configurer.setIgnoreUnresolvablePlaceholders(false);
         return configurer;
     }
--- src/main/java/org/example/touragency/dto/request/RatingDto.java
+++ src/main/java/org/example/touragency/dto/request/RatingDto.java
@@ -1,7 +1,6 @@
 package org.example.touragency.dto.request;
 
 import lombok.*;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
